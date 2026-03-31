93ed030042ed6ff3510a458fa87b86e40122a585
lucas-moia <pessoalucas95@gmail.com>
2026-03-31 19:31:25 -0300

    ajustando o banco de dados

--- api's/src/config/data_base.py
+++ api's/src/config/data_base.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    database_url = os.getenv('DATABASE_URL', "mysql+pymysql://root:fuYzRRvPELwXmqLEKbzZUoEBXyOtcVXE@tramway.proxy.rlwy.net:33501/railway")
+    database_url = os.getenv('DATABASE_URL', "mysql+pymysql://root:oKSoXaZsQrsDPIrqsLioSBoBSckSYtVB@caboose.proxy.rlwy.net:41758/railway")
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
